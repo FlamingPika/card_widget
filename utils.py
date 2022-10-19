11246430b970f9375b760763b596da250c133597
Wilson Thiesman <wilsonthiesman17@gmail.com>
2022-10-19 20:29:32 +0800

    add checking system to validate the returned dictionary

--- utils.py
+++ utils.py
@@ -61,7 +61,10 @@ def generate_desc(path, filename, extension):
     print(f"Total number of files is {len(content)}")
 
     image_list = ['src/discs_daughter.gif', 'src/cups_8.gif', 'src/wands_daughter.gif', 'src/swords_9.gif', 'src/wands_9.gif', 'src/wands_son.gif', 'src/wands_8.gif', 'src/swords_8.gif', 'src/cups_9.gif', 'src/swords_shaman.gif', 'src/discs_9.gif', 'src/wands_10.gif', 'src/discs_priestess.gif', 'src/discs_8.gif', 'src/cups_10.gif', 'src/cups_priestess.gif', 'src/wands_ace.gif', 'src/swords_priestess.gif', 'src/wands_priestess.gif', 'src/discs_3.gif', 'src/cups_shaman.gif', 'src/cups_ace.gif', 'src/discs_2.gif', 'src/swords_daughter.gif', 'src/discs_6.gif', 'src/wands_shaman.gif', 'src/swords_ace.gif', 'src/discs_7.gif', 'src/discs_5.gif', 'src/discs_son.gif', 'src/discs_10.gif', 'src/discs_4.gif', 'src/cups_7.gif', 'src/cups_son.gif', 'src/swords_6.gif', 'src/swords_10.gif', 'src/wands_6.gif', 'src/wands_7.gif', 'src/cups_daughter.gif', 'src/swords_7.gif', 'src/cups_6.gif', 'src/cups_4.gif', 'src/swords_5.gif', 'src/wands_5.gif', 'src/wands_4.gif', 'src/swords_4.gif', 'src/cups_5.gif', 'src/discs_shaman.gif', 'src/discs_ace.gif', 'src/cups_2.gif', 'src/swords_3.gif', 'src/wands_3.gif', 'src/wands_2.gif', 'src/swords_son.gif', 'src/swords_2.gif', 'src/cups_3.gif'];
-
+    for key in content:
+        if key not in image_list:
+            raise Exception("The file format is not inside the list")
+            
     print(content)
 
 
